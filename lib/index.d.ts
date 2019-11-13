70478ca104c75e13236af58333035a214bf5b665
Anis MOKEDDES <AMOKEDDES@externe.generali.fr>
2019-11-13 16:23:22 +0100

    fix(type-def): fix type definition

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,73 +1,89 @@
-import { ErrorRequestHandler, RequestHandler } from 'express';
-import { 
-    Root as joi, 
-    ValidationOptions, 
-    ValidationError, 
-    ValidationResult 
-} from '@hapi/joi';
+import { ErrorRequestHandler, RequestHandler } from "express";
+import {
+  Root as joi,
+  ValidationOptions,
+  ValidationError,
+  ValidationResult
+} from "@hapi/joi";
 
 declare namespace Celebrate {
-    /**
-    * Creates a Celebrate middleware function.
-    */
-    function celebrate(schema: {
-        /**
-         * When `params` is set, `joi` will validate `req.params` with the supplied schema.
-         */
-        params?: object,
-        /**
-         * When `headers` is set, `joi` will validate `req.headers` with the supplied schema.
-         */
-        headers?: object,
-        /**
-         * When `query` is set, `joi` will validate `req.query` with the supplied schema.
-         */
-        query?: object,
-        /**
-         * When `cookies` is set, `joi` will validate `req.cookies` with the supplied schema.
-         */
-        cookies?: object,
-        /**
-         * When `signedCookies` is set, `joi` will validate `req.signedCookies` with the supplied schema.
-         */
-        signedCookies?: object,
-        /**
-         * When `body` is set, `joi` will validate `req.body` with the supplied schema.
-         */
-        body?: object,
-    },  
+  /**
+   * Creates a Celebrate middleware function.
+   */
+  function celebrate(
+    schema: {
+      /**
+       * When `params` is set, `joi` will validate `req.params` with the supplied schema.
+       */
+      params?: object;
+      /**
+       * When `headers` is set, `joi` will validate `req.headers` with the supplied schema.
+       */
+      headers?: object;
+      /**
+       * When `query` is set, `joi` will validate `req.query` with the supplied schema.
+       */
+      query?: object;
+      /**
+       * When `cookies` is set, `joi` will validate `req.cookies` with the supplied schema.
+       */
+      cookies?: object;
+      /**
+       * When `signedCookies` is set, `joi` will validate `req.signedCookies` with the supplied schema.
+       */
+      signedCookies?: object;
+      /**
+       * When `body` is set, `joi` will validate `req.body` with the supplied schema.
+       */
+      body?: object;
+    },
     joiOptions?: ValidationOptions,
     celebrateOptions?: {
-        /**
-         * When `true` uses the entire `req` object as the `context` value during validation. 
-         */
-       reqContext: boolean, 
-    }): RequestHandler
-    /**
-     * Creates a Celebrate error handler middleware function.
-     */
-    function errors(): ErrorRequestHandler;
-    /**
-     * The Joi version Celebrate uses internally.
-     */
-    export const Joi: joi;
-
-    /**
-     * Examines an error object to determine if it originated from the celebrate middleware.
-     */
-    function isCelebrate(err: object): boolean;
-
-    /**
-     * Format a joi error into a standard object
-     */
-    function format(err: ValidationResult<object>, 
-        source: "params" | "headers" | "query" | "cookies" | "signedCookies" | "body", 
-        opts?: { celebrated: boolean }): {
-        meta: {
-            source: "params" | "headers" | "query" | "cookies" | "signedCookies" | "body"
-        },
-        joi: ValidationError,
+      /**
+       * When `true` uses the entire `req` object as the `context` value during validation.
+       */
+      reqContext: boolean;
     }
+  ): RequestHandler;
+  /**
+   * Creates a Celebrate error handler middleware function.
+   */
+  function errors(): ErrorRequestHandler;
+  /**
+   * The Joi version Celebrate uses internally.
+   */
+  export const Joi: joi;
+
+  /**
+   * Examines an error object to determine if it originated from the celebrate middleware.
+   */
+  function isCelebrate(err: object): boolean;
+
+  /**
+   * Format a joi error into a standard object
+   */
+  function format(
+    err: ValidationResult,
+    source:
+      | "params"
+      | "headers"
+      | "query"
+      | "cookies"
+      | "signedCookies"
+      | "body",
+    opts?: { celebrated: boolean }
+  ): {
+    meta: {
+      source:
+        | "params"
+        | "headers"
+        | "query"
+        | "cookies"
+        | "signedCookies"
+        | "body";
+    };
+    joi: ValidationError;
+  };
 }
 
 export = Celebrate;
